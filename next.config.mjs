ea5e027566724c10ac5538c4a31386003ed56337
OhadBaehr <33723454+OhadBaehr@users.noreply.github.com>
2024-07-06 19:59:20 +0300

    Another attempt to solve the caching issue

--- next.config.mjs
+++ next.config.mjs
@@ -12,4 +12,25 @@ export default withBundleAnalyzer({
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+        ],
+      },
+    ];
+  },
 });
